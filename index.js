fc9282860f2e57b3c34de19ecb4fed862558399a
Mansour <maqsoudi99@gmail.com>
2023-02-25 11:10:24 -0500

    Started adding new event listener for upgradeneeded

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Info from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage
+
 const titleInput = document.querySelector("#title");
 const list = document.querySelector("ul");
 const bodyInput = document.querySelector("#body");
@@ -15,12 +17,33 @@ const openRequest = window.indexedDB.open("notes_db", 1);   //request to open ve
 
 //Event handlers
 //error handler signifies that database didn't open successfully
-openRequest.addEventListener("error",  () => console.error("Database failed to open"));
+openRequest.addEventListener("error",  () => 
+    console.error("Database failed to open")
+);
 
-openRequest.addEventListener("success", () => console.log("Database opened successfully"));
+//Success handler
+openRequest.addEventListener("success", () => {
+    console.log("Database opened successfully");
 
-//Store the opened data bases in the db variable
-db = openRequest.result;
+    //Object representing opened database is stored in db variable. Allows us to manipulate the database.
+    db = openRequest.result;
 
-//Display the data alaready in the IDB
-db.displayData();
+    //Display the data already in the IndexDB inside the <ul>
+    displayData();
+});
+
+//Most important handler (ugradeneeded)
+//runs if databse has not already been set up, or if db is opened with a bigger version number than thexisting stored database
+
+//Set up db tables if this has not already been done
+openRequest.addEventListener("upgradeneeded", (e) => {
+    //grab reference to the opened database
+    db = e.target.result;
+
+    //create an objectStore in our databse to store notes and an auto-incrementing key
+    //An objectStore is similar to a 'table' in a relational database
+    const objectStore = db.createObjectStore("notes_os", {keyPath: "id", autoIncrement: true,});
+
+    //Define what data items the objectStore will contain
+    
+})
