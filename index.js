2a270c73643d8a3856ace3b57b6c6f0e4a39f2bc
Mansour <maqsoudi99@gmail.com>
2023-03-03 15:15:59 -0500

    added the addData function and fixed comments

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let db;
 const openRequest = window.indexedDB.open("notes_db", 1);   //request to open version "1" of database called "notes_db"
 
 //Opening a database takes time, you dont want the browser to hang.
-//Therefore database operations happn asychronous, meaning you get notified when there done in the future
+//Therefore database operations happen asychronous, meaning you get notified when they're done in the future
 
 //Event handlers
 //error handler signifies that database didn't open successfully
@@ -32,18 +32,61 @@ openRequest.addEventListener("success", () => {
     displayData();
 });
 
-//Most important handler (ugradeneeded)
-//runs if databse has not already been set up, or if db is opened with a bigger version number than thexisting stored database
+//Most important handler (upgradeneeded)
+//runs if database has not already been set up, or if db is opened with a bigger version number than the existing stored database
 
 //Set up db tables if this has not already been done
 openRequest.addEventListener("upgradeneeded", (e) => {
     //grab reference to the opened database
     db = e.target.result;
 
-    //create an objectStore in our databse to store notes and an auto-incrementing key
+    //create an objectStore in our database to store notes and an auto-incrementing key
     //An objectStore is similar to a 'table' in a relational database
     const objectStore = db.createObjectStore("notes_os", {keyPath: "id", autoIncrement: true,});
 
     //Define what data items the objectStore will contain
+    objectStore.createIndex("title", "title", {unique: false});
+    objectStore.createIndex("body", "body", {unique: false});
+    console.log("Database setup complete");
+    //So whe adding a record to the database, each one will be represented as an object that has title, body and id.
     
-})
+});
+
+//create a submit event handler so when form is submitted, addData() function will run
+form.addEventListener("submit", addData);
+
+//Define the addData() function
+function addData(e){
+    //prevent default, we don't want the for to submit in conventional way (this would cause page refresh and spoil the experience)
+    e.preventDefault();
+    //grab values entered into the form fields and store them in an object ready for being inserted into the DB
+    const newItem = {title: titleInput.value, body: bodyInput.value};
+
+    //open a read/write db transaction on the notes_os object store, ready for adding the data
+    const transaction = db.transaction(["notes_os"], "readwrite");
+
+    //call an object store that's already been added to the database (access object store using objectStore() method, save result in objectStore variable)
+    const objectStore = transaction.objectStore("notes_os");
+
+    //Make a request to add our newItem object to the object store. (Add new record to db using .add() method. This creates a request object, in the same fashion we've seen before.)
+    const addRequest = objectStore.add(newItem);
+
+
+    //Event handlers to run at critical points in the lifecycle
+    addRequest.addEventListener("success", () => {
+        //clear the form, ready for adding the next entry, when the request has succeeded
+        titleInput.value = "";
+        bodyInput.value = "";
+    });
+    //Report on success of the transaction completing, when everything is done
+    transaction.addEventListener("complete", () => {
+        console.log("Transaction completed: database modification finished.");
+
+        //update the display of data to show the newly added item, by running displayData() again.
+        displayData();
+    });
+
+    transaction.addEventListener("error", () => 
+        console.log("Transaction not opened due to error")
+    );
+}
